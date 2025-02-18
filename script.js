03ecf93634718b8cb2c6408b2cd2e60dc7ed64ce
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 18:35:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name:string, age: number){
+		this.name = name;
+		this.age= age;
+	}
+	get name(name){
+		return name;
+	}
 
-class Student extends Person {}
+	set age(newAge){
+		this.age= newAge;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	constructor(name:string, age: number){
+		super(name,age);
+	}
+	super.name("Divya");
+}
 
+class Teacher extends Person {
+	constructor(name:string, age: number){
+		super(name,age);
+	}
+	super.age(23);
+}
+const window = new Person();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
